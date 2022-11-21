65424787a4a0fd922a1c4d0cc1792a83439224de
sisodev <bala.mg88@gmail.com>
2022-11-21 21:47:18 +0530

    next commit 1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  exportTrailingSlash: true,
+  exportPathMap: function(){
+    return {
+      "/": {page: "/"}
+    }
+  }
 }
 
 module.exports = nextConfig
